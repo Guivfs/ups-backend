05d6fd223fee7299594e3357358909434aeecc53
Guivfs <guizinho.vfs@gmail.com>
2024-10-08 08:51:47 -0300

    Deploy backend

---
+++ model/User.js
@@ -0,0 +1,11 @@
+class UserModel {
+    constructor(nomeUsuario, userUsuario, senhaUsuario, cepUsuario, emailUsuario) {
+      this.nomeUsuario = nomeUsuario;
+      this.userUsuario = userUsuario;
+      this.senhaUsuario = senhaUsuario;
+      this.cepUsuario = cepUsuario;
+      this.emailUsuario = emailUsuario;
+    }
+  }
+  
+  module.exports = UserModel;  
